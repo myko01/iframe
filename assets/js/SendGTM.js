91383f580bcfaa089278853d69c267a90860b01e
myko01 <myko01@betssongroup.com>
2021-04-27 17:03:35 +0300

    add things

--- assets/js/SendGTM.js
+++ assets/js/SendGTM.js
@@ -1,14 +1,27 @@
 const ORIGIN = 'https://myko01.github.io';
 
 function sendGtmService(data) {
+
+    if (!inIframe()) {
+        return;
+    }
+
     const message = {
         isGtmEvent: true,
-        data
+        ...data
     };
 
     try {
-        parent.postMessage(message, ORIGIN);
+        parent.postMessage(JSON.stringify(message), ORIGIN);
     } catch (e) {
         window.console && window.console.log(e);
+    }
+}
+
+function inIframe() {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
     }
 }
